62efc4a0a7fb68f02851fc1f11d5d0f0c13b7d89
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2020-12-26 16:04:55 +0200

    add only musthave properties withou useless by Rest Operator

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,10 +2,14 @@ import React from 'react';
 import TodoListItem from "./todo-list-item";
 
 const TodoList = ({TodoData}) => {
-
-    const {id, ...itemObj} = TodoData;
-
-    const ListItem = TodoData.map((items)=> <li key={items.id}><TodoListItem {...itemObj}/></li>)
+    const ListItem = TodoData.map((items)=>{
+        const {id, ...itemObj} = items;
+        return (
+            <li key={id}>
+                <TodoListItem {...itemObj}/>
+            </li>
+        );
+    });
 
     return (
         <div>
