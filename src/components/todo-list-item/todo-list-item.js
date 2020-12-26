cb2e07f47d01e1e2cd90ea30a1287b3a1b73a43b
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2020-12-26 22:30:33 +0200

    state reload

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -5,41 +5,49 @@ import './todo-list-item.css'
 export default class TodoListItem extends Component {
 
     state = {
-        done: false
+        done: false,
+        important: false
     }
 
     onClickListItem = () => {
-        this.setState({
-            done: true
+        this.setState((state)=>{
+            return {
+                done: !state.done
+            }
         })
     }
 
+    OnMarkImportant = () => {
+        this.setState((state) => {
+           return{
+               important: !state.important
+           }
+        })
+    }
     render() {
 
-        const {label, important = false} = this.props;
-        const {done} = this.state;
+        const {label} = this.props;
+        const {done, important} = this.state;
 
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
 
         let classNames = 'todo-list-item';
         if (done) {
             classNames += ' done';
         }
-
+        if(important){
+            classNames += ' important'
+        }
         return (
             <span className={classNames}>
       <span
           className="todo-list-item-label"
-          style={style}
           onClick={this.onClickListItem}>
         {label}
       </span>
 
       <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
+              className="btn btn-outline-success btn-sm float-right"
+              onClick={this.OnMarkImportant}>
         <i className="fa fa-exclamation"/>
       </button>
 
